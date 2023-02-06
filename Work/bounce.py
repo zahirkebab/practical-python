a92a2cf41a912cc47c456c6d4580577a87484362
zahirkebab <93675640+zahirkebab@users.noreply.github.com>
2023-02-06 17:00:47 +0100

    1.03

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
 # bounce.py
 #
 # Exercise 1.5
+y=100.0
+for x in range(10):
+    y=y*0.6
+    print(round(y,3))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+total_months = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 109
+extra_payment = 1000
+
+while principal > 0:
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+
+    if extra_payment_start_month < total_months < extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    total_months += 1
+
+    print(total_months, ' ', total_paid,' ',principal)
+
+#print('Total paid', total_paid, ' ', total_months)
